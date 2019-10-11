3958fc92928057c9cb7a45c3cc9b30e24666ec4d
Łukasz Bieszczad <l.bieszczad@clearcode.cc>
2019-10-11 09:58:08 +0200

    Handle responses in thread and command in a middle of a msg

--- main/bot.go
+++ main/bot.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/nlopes/slack"
@@ -15,24 +16,28 @@ func getenv(name string) string {
 	return v
 }
 
+func handleRequest(rtm *slack.RTM, ev *slack.MessageEvent) {
+	info := rtm.GetInfo()
+	text := ev.Text
+	text = strings.TrimSpace(text)
+	text = strings.ToLower(text)
+	match, _ := regexp.MatchString("!excuse", text)
+
+	if ev.User != info.User.ID && match {
+		excuse := GetExcuse()
+		rtm.SendMessage(rtm.NewOutgoingMessage(excuse, ev.Channel, slack.RTMsgOptionTS(ev.ThreadTimestamp)))
+		println("Sent: '" + excuse + "' to " + ev.Channel)
+	}
+}
+
 func beBot(rtm *slack.RTM) {
 Loop:
 	for {
 		select {
 		case msg := <-rtm.IncomingEvents:
-			println("Event received")
-			println(msg.Type)
 			switch ev := msg.Data.(type) {
 			case *slack.MessageEvent:
-				info := rtm.GetInfo()
-				text := ev.Text
-				text = strings.TrimSpace(text)
-				text = strings.ToLower(text)
-
-				if ev.User != info.User.ID && text == "!excuse" {
-					excuse := GetExcuse()
-					rtm.SendMessage(rtm.NewOutgoingMessage(excuse, ev.Channel))
-				}
+				handleRequest(rtm, ev)
 			case *slack.RTMError:
 				print("Got slack error: " + ev.Error())
 			case *slack.InvalidAuthEvent:
